d085b22a1c41011206577ba94ef413fa47522161
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-15 21:33:56 +0200

    Update mgpt.js

--- mgpt.js
+++ mgpt.js
@@ -6,8 +6,8 @@ PGPTConversation.onMessage((msg) => {
       const ms = msg.container.getElementsByTagName("mgpt")
       console.log(ms)
       for (let m = 0; m < ms.length; m++) {
-        ms[m].innerHTML = `\<div id="t"\>\</div\>\<iframe>\<script src="https://cdnjs.cloudflare.com/ajax/libs/abcjs/6.2.2/abcjs-basic-min.min.js" integrity="sha512-REXLcx385NL3ZWosam5LzLJoA0AkfeVdW94IQURSM/Gl6EyrS0hS3LtqR094d00lsicEVAprWA5SUTD5oljapg==" crossorigin="anonymous" referrerpolicy="no-referrer"\>\</script\>\<script\>	var myContext = new AudioContext();	var synth = new ABCJS.synth.CreateSynth();	var visualObj = ABCJS.renderAbc(document.getElementById("t"), "${ms[m].innerHTML}");	synth.init({		audioContext: myContext,		visualObj: visualObj[0],		millisecondsPerMeasure: 500,		options: {			soundFontUrl: "https://paulrosen.github.io/midi-js-soundfonts/abcjs/",			pan: [ -0.3, 0.3 ] 		}	}).then(function (results) {		synth.prime().then((res) =\> {			synth.start()		})	}).catch(function (reason) {		console.log(reason)	});\</script\>\</iframe\>`
-      }
-    })
+	let mus = ms[m].innerHTML
+	ms[m].innerHTML = '<div id="t"></div>'
+        MusicGPTLoadMusic(ms[m].getElementById("t"), mus)
   }
 }, window.parent.document)
