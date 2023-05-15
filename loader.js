8f6742d41493a281c7ee8a4c97f49ec3247b6164
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-15 21:31:15 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,20 @@
-
+function MusicGptLoadMusic(el) {
+var myContext = new AudioContext();
+var synth = new ABCJS.synth.CreateSynth();
+var visualObj = ABCJS.renderAbc(el, "${ms[m].innerHTML}");
+synth.init({
+  audioContext: myContext,
+  visualObj: visualObj[0],
+  millisecondsPerMeasure: 500,
+  options: {
+    soundFontUrl: "https://paulrosen.github.io/midi-js-soundfonts/abcjs/",
+    pan: [ -0.3, 0.3 ] 
+  }
+}).then(function (results) {
+  synth.prime().then((res) => {
+    synth.start()
+  })
+}).catch(function (reason) {
+  console.log(reason)
+});
+}
